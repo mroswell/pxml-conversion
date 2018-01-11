6ba3314bdf810ba482b54436d2357b98ea20e2fa
Marjorie Roswell <mroswell+github@gmail.com>
2018-01-11 17:16:46 -0500

    write cleaner python code

--- run.py
+++ run.py
@@ -1,55 +1,63 @@
 #!/usr/bin/env python
 #  coding: utf8
 
-import os, base64
+import os
 import xml.etree.ElementTree as et
+import base64
 import csv
 
 ns = {'p': 'http://www.palantirtech.com/pg/schema/import/'}
 count = 0
 
-def process(filepath):
-    ret = []
-    ret.append(path)
 
+def process(filepath):
     tree = et.parse(filepath)
     root = tree.getroot()
-    ret.append('' if root.find('.//p:propertyComponent[@type="TITLE"]/p:propertyData', ns) == None else root.find('.//p:propertyComponent[@type="TITLE"]/p:propertyData', ns).text)
-    ret.append('' if root.find('.//p:noteData', ns) == None else root.find('.//p:noteData', ns).text)
-    ret.append('' if root.find('.//p:propertyData', ns) == None else root.find('.//p:propertyData', ns).text)
+
+    ret = []
+    ret.append(filepath)
+    ret.append('' if root.find('.//p:propertyComponent[@type="TITLE"]/p:propertyData', ns) is None else root.find(
+        './/p:propertyComponent[@type="TITLE"]/p:propertyData', ns).text)
+    ret.append('' if root.find('.//p:noteData', ns) is None else root.find('.//p:noteData', ns).text)
+    ret.append('' if root.find('.//p:propertyData', ns) is None else root.find('.//p:propertyData', ns).text)
 
     medias = root.findall('.//p:media[@mediaType="Text File"]', ns)
     for media in medias:
-        ret.append('' if media.attrib['mediaType'] == None else media.attrib['mediaType'])
-        ret.append('' if media.find('./p:mediaTitle', ns) == None else media.find('./p:mediaTitle', ns).text)
-        text = '' if media.find('./p:mediaData', ns) == None else media.find('./p:mediaData', ns).text
-        ret.append(text)
-        text_decoded = base64.b64decode(text)
-        text_decoded = text_decoded.decode('utf8')
+        ret.append('' if media.attrib['mediaType'] is None else media.attrib['mediaType'])
+        ret.append('' if media.find('./p:mediaTitle', ns) is None else media.find('./p:mediaTitle', ns).text)
+
+        orig_text = '' if media.find('./p:mediaData', ns) is None else media.find('./p:mediaData', ns).text
+        ret.append(orig_text)
+        text_utf = base64.b64decode(orig_text)
+        ret.append(text_utf)
+        text_decoded = text_utf.decode('utf8')
         ret.append(text_decoded)
+
         dataSourceRecordNode = media.find('.//p:dataSourceRecord', ns)
-        if dataSourceRecordNode != None:
+        if dataSourceRecordNode is not None:
             ret.append(et.tostring(dataSourceRecordNode))
         else:
             ret.append('')
     return ret
 
+
 data = []
-    
-for root,dirs,files in os.walk('../xml'):
+
+for root, dirs, files in os.walk('../xml'):
     for file in files:
         path = root + '/' + file
-        # Remove extraneous xml files created by PyCharm IDE
-        if path.endswith('.xml') and path.find('.idea') == -1:
-            print (path)
-            ret = process(path)
-            data.append(ret)
+        # Exclude extraneous xml files created by PyCharm IDE
+        if path.endswith('.xml') and '.idea' not in path:
+            processed = process(path)
+            data.append(processed)
             count += 1
-            print (ret)
+            print (processed)
 
 with open('output.csv', 'w', encoding='utf-8') as csvfile:
-    w = csv.writer(csvfile, delimiter=',', quotechar = '"', quoting=csv.QUOTE_NONNUMERIC )
-    headerwriter = csv.DictWriter(open('output.csv','wt'), ['filename', 'title', 'noteData', 'propertyData', 'mediaType', 'mediaTitle', 'mediaData', 'mediaData_decoded','dataSourceRecord'])
+    w = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
+    headers = ['filename', 'title', 'noteData', 'propertyData', 'mediaType', 'mediaTitle', 'mediaData',
+               'mediaData_utf8', 'mediaData_decoded', 'dataSourceRecord']
+    headerwriter = csv.DictWriter(open('output.csv', 'wt'), headers)
     headerwriter.writeheader()
 
     for line in data:
